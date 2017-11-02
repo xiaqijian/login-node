0e56299196ebeb99cd69505d36c263c8fea5d35a
xiaqijian <2817062995@qq.com>
2017-11-02 16:18:58 +0800

    add ajax

--- v1/app.js
+++ v1/app.js
@@ -46,7 +46,7 @@ app.use(function(req,res,next){
 	// 	res.redirect("/home");
 	// }else {
 		console.log(req.cookies.name)
-		if(!req.cookies.name && url != "/login"){
+		if(!req.cookies.name && url != "/login" && url != "/register"){
 			console.log('3333')
 			return res.redirect("/login");
 		}else {
@@ -62,32 +62,41 @@ app.use(function(req,res,next){
 
 
 app.route('/')
-	.get(function(req,res){
-			if(req.cookies.name=='value'){
-				res.redirect('home')
-			}else {
-				res.render('index')
-			}
+	.get(function(req,res){			
+		res.render('index')
 	})
 
 app.route('/login')
 	.get(function(req,res){
+		if(req.cookies.name){
+			res.redirect('/home');
+			return;
+		}
 		res.render('login',{ title:'用户登录'})
 	})
 	.post(function(req,res){
 		var email = req.body.email;
 		var password = req.body.password;
-		console.log(password);
 		queryMysql(email,password,function(data,name){
-			console.log(data)
-			console.log(typeof data)
 			if(data==="登陆成功"){
 				req.session.islogin = true;
 				req.session.username = name;
 				res.cookie('name', 'value');
-				res.redirect('/home')
-			}else {
-				res.send(data)
+				res.json({
+					status:1,
+					text:'登录成功'
+				})
+			}else if(data==="没有该用户"){
+				res.json({
+					status:-1,
+					text:'没有该用户'
+				})
+			}
+			else{
+				res.json({
+					status:0,
+					text:'密码错误'
+				})
 			}
 			
 		});
@@ -102,9 +111,19 @@ app.route('/register')
    		var email = req.body.email;
    		var username = req.body.username;
    		var password = req.body.password;
-   		addMysql(email,username,password);
+   		onmysql(email,function(data){
+   			console.log('cha')
+   			if(data.status==200){
+   				addMysql(email,username,password,function(data1){
+		   			res.json(data1)
+		   		});
+   			}else {
+   				res.json(data)
+   			}
+   		})
+   		
    		// console.log(req.body);
-   		res.redirect('/login')
+   		// res.redirect('/login')
    })
 
 app.route('/home')
@@ -125,43 +144,71 @@ app.route('/lists')
 		res.render('lists',{ name:req.session.username})
 	})
 
-function addMysql(email,name,password){
+function addMysql(email,name,password,callback){
 	var  addSql = 'INSERT INTO test1(id,email,name,password) VALUES(0,?,?,?)';
 	var  addSqlParams = [email, name,password];
 	pool.getConnection(function (err, conn) {
-	    if (err) console.log("POOL ==> " + err);
+	    // if (err) console.log("POOL ==> " + err);
 	    conn.query(addSql,addSqlParams,function(err,rows){
 	        if (err){
 	        	console.log('添加新用户失败')
+	        	callback({
+	        		status:-1,
+	        		text:'添加新用户失败'
+	        	})
 	        }else {
-	        	console.log('成功新添加用户'+name)
+	        	// console.log('成功新添加用户'+name)
+	        	callback({
+	        		status:1,
+	        		text:'新用户成功'
+	        	})
 	        }
 	        conn.release();
 	    });
 	});
 }
 
-function queryMysql(email,password,callback){
+function onmysql(email,callback){
 	var querymysql = 'select * from test1 where email="'+email+'";'
 	console.log(querymysql);
+	pool.getConnection(function(err,conn){
+		conn.query(querymysql,function(err,rows){
+			if (rows.length==0) {
+				callback({
+					status:200,
+					text:'没有该用户'
+				})
+			}else {
+				callback({
+					status:0,
+					text:'换个邮箱注册，此邮箱被注册'
+				})
+			}
+			conn.release();
+		})
+	})
+}
+
+function queryMysql(email,password,callback){
+	var querymysql = 'select * from test1 where email="'+email+'";'
 	pool.getConnection(function(err,conn){
 		console.log('4444444')
 		// if (err) console.log("POOL ==> " + err);
 	    conn.query(querymysql,function(err,rows){
 	        if (err){
-	        	console.log('查询失败')
+	        	
 	        	callback('数据库返回错误')
 	        }else {
 	        	if (rows.length===0) {
-	        		console.log('用户为空')
+	        		callback('没有该用户')
 	        	}else {
 	        		console.log(rows);
 		        	if(rows[0].password===password){
 
 		        		callback('登陆成功',rows[0].name)
-		        		console.log('登录成功')
+		        		
 		        	}else{
-		        		console.log('密码错误')
+		        		
 		        		callback('密码错误','')
 		        	}
 	        	}
